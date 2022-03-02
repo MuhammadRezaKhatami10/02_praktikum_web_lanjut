4d1c452526589d0bb843800d71fab78b6aec006a
MuhammadRezaKhatami10 <2041720076@student.polinema.ac.id>
2022-03-02 23:01:39 +0700

    perubahan route pertama

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "NIM : 2041720076<br>Nama : Muhammad Reza Khatami<br>Kelas : TI 2C";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Ini adalah halaman artikel dengan ID $id";
 });
